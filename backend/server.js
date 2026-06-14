f3812ffb9cd55b663fbd56e65979415e9c6180ad
Rahman Mahmutović <mahmutovicrahman5@gmail.com>
2026-06-14 18:01:55 +0200

    Refactor server.js to handle production environment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
-import cors from "cors"
+import path from "path"
+import { fileURLToPath } from "url"
 import { connectDB } from "./config/db.js"
 import productRoutes from "./routes/product.route.js"
 import authRoutes from "./routes/auth.route.js"
@@ -8,12 +9,13 @@ import featuredRoutes from "./routes/featured.route.js"
 import searchRoutes from "./routes/search.route.js"
 
 dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.use(cors({
-  origin: process.env.ALLOWED_ORIGIN
-}))
 app.use(express.json())
 
 app.use("/api/products", productRoutes)
@@ -21,7 +23,14 @@ app.use("/api/auth", authRoutes)
 app.use("/api/featured", featuredRoutes)
 app.use("/api/search", searchRoutes)
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"))
+  })
+}
+
 app.listen(PORT, () => {
-  connectDB()
   console.log("Server started at http://localhost:" + PORT)
+  connectDB()
 })
